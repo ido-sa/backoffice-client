a2061b073c25adab09397d13e959cfbc9c971f3f
ido-sa <isarussy@gmail.com>
2025-09-02 16:51:32 +0300

    fix: remove unused theme parameters in TransactionsPage styles

--- src/pages/operations/transactions/TransactionsPage.styles.ts
+++ src/pages/operations/transactions/TransactionsPage.styles.ts
@@ -1,13 +1,13 @@
 import { styled } from '@mui/material/styles'
 import { Box, Grid, Alert } from '@mui/material'
 
-export const StyledPageContainer = styled(Box)(({ theme }) => ({
+export const StyledPageContainer = styled(Box)(() => ({
   height: '100%',
   display: 'flex',
   flexDirection: 'column',
 }))
 
-export const StyledGridContainer = styled(Grid)(({ theme }) => ({
+export const StyledGridContainer = styled(Grid)(() => ({
   height: '100%',
 }))
 
@@ -28,10 +28,10 @@ export const StyledTablesContainer = styled(Box)(({ theme }) => ({
   marginTop: theme.spacing(1),
 }))
 
-export const StyledErrorAlert = styled(Alert)(({ theme }) => ({
+export const StyledErrorAlert = styled(Alert)(() => ({
   width: '100%',
 }))
 
-export const StyledSuccessAlert = styled(Alert)(({ theme }) => ({
+export const StyledSuccessAlert = styled(Alert)(() => ({
   width: '100%',
 }))
